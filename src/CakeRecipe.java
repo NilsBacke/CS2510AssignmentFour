2472ab3a8778cb7727e6ee198a957c635ce4f903
Nils Backe <nilsbacke@Nilss-MacBook-Pro.local>
2018-10-03 09:33:09 -0400

    fixed same recipe

--- src/CakeRecipe.java
+++ src/CakeRecipe.java
@@ -1,6 +1,11 @@
+import tester.*;
+
+// represents all CakeRecipes
 class CakeRecipe {
   double flour, sugar, eggs, butter, milk; // weights in ounces
-  
+
+  // constructor that represents as the amount of flour, sugar, eggs, butter and
+  // milk in the recipe
   CakeRecipe(double flour, double sugar, double eggs, double butter, double milk) {
     if (flour != sugar) {
       throw new IllegalArgumentException("Weight of flour must be equal to weight of sugar.");
@@ -9,7 +14,8 @@ class CakeRecipe {
       throw new IllegalArgumentException("Weight of eggs must be equal to weight of butter.");
     }
     if (eggs + milk != sugar) {
-      throw new IllegalArgumentException("Weight of eggs plus the weight of milk must equal the weight of sugar.");
+      throw new IllegalArgumentException(
+          "Weight of eggs plus the weight of milk must equal the weight of sugar.");
     }
     this.flour = flour;
     this.sugar = sugar;
@@ -17,14 +23,18 @@ class CakeRecipe {
     this.butter = butter;
     this.milk = milk;
   }
-  
+
+  // constructor that only requires one to enter the amount of flour, eggs and
+  // milk
   CakeRecipe(double flour, double eggs, double milk) {
     this(flour, flour, eggs, eggs, milk);
   }
-  
+
+  // constructor that takes in the flour, eggs and milk
+  // as volumes rather than weights and a boolean flag areVolumes
   CakeRecipe(double flour, double eggs, double milk, boolean areVolumes) {
     this(flour, eggs, milk);
-    
+
     if (areVolumes) {
       this.flour = flour * 4.25;
       this.sugar = flour * 7;
@@ -33,12 +43,24 @@ class CakeRecipe {
       this.milk = milk * 8;
     }
   }
-  
+
+  // detects if the two CakeRecipes are the same
   boolean sameRecipe(CakeRecipe other) {
-    return this.flour - other.flour < .001 &&
-        this.sugar - other.sugar < .001 &&
-        this.eggs - other.eggs < .001 &&
-        this.butter - other.butter < .001 &&
-        this.milk - other.milk < .001;
+    return Math.abs(this.flour - other.flour) <= .001 && Math.abs(this.sugar - other.sugar) <= .001
+        && Math.abs(this.eggs - other.eggs) <= .001 && Math.abs(this.butter - other.butter) <= .001
+        && Math.abs(this.milk - other.milk) <= .001;
+  }
+}
+
+// represents all types of CakeRecipes
+class ExamplesCakeRecipe {
+  CakeRecipe redVelvet = new CakeRecipe(10, 10, 5, 5, 5);
+  CakeRecipe redVelvetChocoChip = new CakeRecipe(10, 10, 5, 5, 5);
+  CakeRecipe chocolate = new CakeRecipe(22, 22, 11, 11, 11);
+
+  // testing sameRecipe
+  boolean testSameRecipe(Tester t) {
+    return t.checkExpect(this.redVelvet.sameRecipe(this.chocolate), false)
+        && t.checkExpect(this.redVelvet.sameRecipe(this.redVelvetChocoChip), true);
   }
 }
