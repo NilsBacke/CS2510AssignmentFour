15af5394598d0042db4b7c2725263c6dc810ec51
Nils Backe <nilsbacke@Nilss-MacBook-Pro.local>
2018-10-03 20:49:46 -0400

    final copy

--- src/CakeRecipe.java
+++ src/CakeRecipe.java
@@ -2,18 +2,22 @@ import tester.*;
 
 // represents all CakeRecipes
 class CakeRecipe {
-  double flour, sugar, eggs, butter, milk; // weights in ounces
+  double flour;
+  double sugar;
+  double eggs;
+  double butter;
+  double milk; // weights in ounces
 
   // constructor that represents as the amount of flour, sugar, eggs, butter and
   // milk in the recipe
   CakeRecipe(double flour, double sugar, double eggs, double butter, double milk) {
-    if (flour != sugar) {
+    if (!(Math.abs(flour - sugar) <= 0.001)) {
       throw new IllegalArgumentException("Weight of flour must be equal to weight of sugar.");
     }
-    if (eggs != butter) {
+    if (!(Math.abs(eggs - butter) <= 0.001)) {
       throw new IllegalArgumentException("Weight of eggs must be equal to weight of butter.");
     }
-    if (eggs + milk != sugar) {
+    if (!((Math.abs(eggs + milk) - sugar) <= 0.001)) {
       throw new IllegalArgumentException(
           "Weight of eggs plus the weight of milk must equal the weight of sugar.");
     }
@@ -57,10 +61,22 @@ class ExamplesCakeRecipe {
   CakeRecipe redVelvet = new CakeRecipe(10, 10, 5, 5, 5);
   CakeRecipe redVelvetChocoChip = new CakeRecipe(10, 10, 5, 5, 5);
   CakeRecipe chocolate = new CakeRecipe(22, 22, 11, 11, 11);
+  CakeRecipe vanilla = new CakeRecipe(3, 3, 1, 1, 2);
+  CakeRecipe confetti = new CakeRecipe(5, 4, 1, true);
+  CakeRecipe candyCane = new CakeRecipe(5, 4, 1, true);
+  CakeRecipe lemon = new CakeRecipe(12, 5, 7, false);
+  CakeRecipe chocolateAndVanilla = new CakeRecipe(17, 8, 9);
+  CakeRecipe blueberry = new CakeRecipe(17, 8, 9);
+  CakeRecipe strawberryShortcake = new CakeRecipe(66, 12, 54);
 
   // testing sameRecipe
   boolean testSameRecipe(Tester t) {
     return t.checkExpect(this.redVelvet.sameRecipe(this.chocolate), false)
-        && t.checkExpect(this.redVelvet.sameRecipe(this.redVelvetChocoChip), true);
+        && t.checkExpect(this.redVelvet.sameRecipe(this.redVelvetChocoChip), true)
+        && t.checkExpect(this.chocolate.sameRecipe(this.vanilla), false)
+        && t.checkExpect(this.confetti.sameRecipe(this.candyCane), true)
+        && t.checkExpect(this.candyCane.sameRecipe(this.lemon), false)
+        && t.checkExpect(this.blueberry.sameRecipe(this.chocolateAndVanilla), true)
+        && t.checkExpect(this.chocolateAndVanilla.sameRecipe(this.strawberryShortcake), false);
   }
 }
